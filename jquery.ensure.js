718f77365f0a58d8f0e4889f3a98ff6cda541225
Adam McCrea <adam@adamlogic.com>
2008-10-17 09:11:45 -0400

    wrap jQuery find rather than init -- better access to the context(prevObject)

--- jquery.ensure.js
+++ jquery.ensure.js
@@ -1,11 +1,11 @@
 (function($) {
 
 $.fn.ensure = function(fn) {
-  var args = $.makeArray(arguments), selector = this.selector, context = this.context;
+  var args = $.makeArray(arguments), selector = this.selector, prevObject = this.prevObject;
   args.shift();
 
   // id is the array position of this action
-  var id = $.ensure.actions.push({fn:fn, args:args, selector:selector, context:context}) - 1;
+  var id = $.ensure.actions.push({fn:fn, args:args, selector:selector, prevObject:prevObject}) - 1;
 
   this.data('ensured_action_' + id, true);
 
@@ -17,7 +17,7 @@ $.ensure = {
 
   run: function() {
     $.each(this.actions, function(id, action) {
-      $(action.selector, action.context).each(function() {
+      $(action.prevObject).find(action.selector).each(function() {
         if (!$(this).data('ensured_action_' + id)) {
           $(this)[action.fn].apply($(this), action.args);
           $(this).data('ensured_action_' + id, true);
@@ -40,25 +40,16 @@ $.fn.domManip = function() {
   return r;
 };
 
-// Create a new init method that exposes two new properties: selector and context
-var init = $.prototype.init;
-$.prototype.init = function(a,c) {
-	// Call the original init and save the result
-	var r = init.apply(this, arguments);
+// Create a new find method that exposes the most recent selector used
+// (find is called inside $(selector))
+var find = $.fn.find;
+$.fn.find = function(selector) {
+	// Call the original find and save the result
+	var r = find.apply(this, arguments);
 	
-	// Copy over properties if they exist already
-	if (a && a.selector)
-		r.context = a.context, r.selector = a.selector;
-		
-	// Set properties
-	if ( typeof a == 'string' )
-		r.context = c || document, r.selector = a;
+  r.selector = selector;
 	
-	// Return the result
 	return r;
 };
-
-// Give the init function the jQuery prototype for later instantiation (needed after Rev 4091)
-$.prototype.init.prototype = $.prototype;
 
 })(jQuery);
