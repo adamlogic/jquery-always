4636fd50ad8ab525d2b8c1808fe6a69eb9760396
Adam McCrea <adam@adamlogic.com>
2009-02-17 14:22:16 -0500

    Changed name and API -- .always().addClass('always_added')

---
+++ jquery.always.js
@@ -0,0 +1,68 @@
+(function($) {
+
+$.fn.always = function() {
+  return new $.always.init(this);
+};
+
+$.always = {};
+$.always.init = function(original) {
+  this.selector = original.selector,
+  this.context = original.context ? $(original.context) : $('body');
+  this.original = original;
+};
+
+$.always.registerFunction = function(functionName) {
+  $.always.init.prototype[functionName] = function() {
+    var selector = this.selector,
+        args = arguments;
+
+    this.context.bind('always.inserted', function(e) {
+      // find matching descendent elements, including the element itself
+      var elems = $(e.target).find('*').andSelf().filter(selector);
+
+      // apply the action to all matched elements
+      elems.each(function(i, elem) {
+        $.fn[functionName].apply($(elem), args);
+      });
+    });
+
+    $.fn[functionName].apply(this.original, args);
+
+    return this;
+  }
+};
+
+$(function() {
+  for (var functionName in $.fn) {
+    $.always.registerFunction(functionName);
+  }
+});
+
+// Wrap the jQuery domManip method to trigger the always.inserted event
+var domManip = $.fn.domManip
+$.fn.domManip = function() {
+
+  var callback = arguments[2],
+      elems = arguments[0],
+      alreadyOnPage = false;
+
+  $.each(elems, function(i, elem) {
+    if (elem.length) elem = elem[0];
+    if (elem.parentNode) alreadyOnPage = true;
+  });
+
+  arguments[2] = function(elem) {
+    var first = elem.firstChild; // don't want the document-fragment
+
+    callback.call(this, elem);
+
+    if (elem.nodeType != 3 && !alreadyOnPage) {
+      $(first).trigger('always.inserted');
+    }
+  };
+
+  // Call the original method
+  return domManip.apply(this, arguments);
+};
+
+})(jQuery);
